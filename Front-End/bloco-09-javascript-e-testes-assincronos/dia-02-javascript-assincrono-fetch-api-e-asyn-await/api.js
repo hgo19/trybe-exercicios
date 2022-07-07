89d7dc80cf2352b5ea8b14d00461b88c52750dca
Hugo Leonardo <huleonardo19@gmail.com>
2022-07-07 19:47:38 -0300

    exericio 9 dia 02

---
+++ Front-End/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyn-await/api.js
@@ -0,0 +1,27 @@
+const append = async (param) => {
+  const convertUsdUrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json';
+  const response = await fetch(convertUsdUrl);
+  const data = await response.json();
+  const brl = data.usd.brl;
+  const listContainer = document.querySelector('#assets-container');
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `${param.id} (${param.symbol}): ${parseFloat(param.priceUsd * brl).toFixed(2)}`;
+  listContainer.appendChild(listItem);
+}
+
+const setAsset = async (param) => {
+  const apiUrl = 'https://api.coincap.io/v2/assets';
+  const response = await fetch(apiUrl);
+  const result = await response.json();
+  const data = result.data;
+  const selectedCoin = data.find((e) => e.rank === param);
+  append(selectedCoin);
+}
+
+const finalProcess = () => {
+  for (let i = 1; i <= 10; i += 1) {
+    setAsset(`${i}`);
+  }
+}
+
+finalProcess();
